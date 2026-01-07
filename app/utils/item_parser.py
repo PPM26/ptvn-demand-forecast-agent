79d1759cf7833ec86c5668040294494a6fd23003
PPM226 <apinya.boony@ku.th>
2026-01-07 14:09:42 +0700

    delete unuse file

--- app/utils/item_parser.py
+++
@@ -1,7 +0,0 @@
-def fix_item_format(item: str) -> str:
-    """
-    Normalizes the item string by converting to lowercase and removing all spaces.
-    """
-    if not item:
-        return item
-    return item.lower().replace(" ", "")
